93e346503b30b45ff473f03d783c8ddfd2f816fa
pankaja-shree <pankajashree@gmail.com>
2018-01-31 11:41:13 +0530

    add /about route

--- src/router.js
+++ src/router.js
@@ -26,6 +26,7 @@ export default new VueRouter({
 
   routes: [
     { path: '/', component: load('Hello') },
+    { path: '/about', component: load('About') },
 
     // Always leave this last one
     { path: '*', component: load('Error404') } // Not found
